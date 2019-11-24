f8b3d87567908f082c1aa4f5d26cf54bd2d39c8b
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-11-24 15:59:49 -0500

    Complete

---
+++ answers/exercise3.sql
@@ -0,0 +1,7 @@
+create table Users (
+	user_id INT,
+	LastName VARCHAR(255),
+	FirstName VARCHAR(255),
+	Address VARCHAR(255),
+	City VARCHAR(255)
+);
---
+++ answers/exercise8.sql
@@ -0,0 +1,2 @@
+INSERT INTO STUDENTS (StudentName, Address, City, PostalCode, Country) VALUES
+ ('Jane Doe', '57 Union Street', 'Glasgow, Scotland', 'G13RB');
